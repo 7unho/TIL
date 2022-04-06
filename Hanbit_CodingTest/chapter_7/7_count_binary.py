e2fa384eb3204e1e45759b4fbd763453dee32153
april2nds <rlawnsgh8395@naver.com>
2022-04-06 19:18:31 +0900

    0406 practice

--- Hanbit_CodingTest/chapter_7/7_count_binary.py
+++ Hanbit_CodingTest/chapter_7/7_count_binary.py
@@ -20,7 +20,7 @@ def count_by_range(array, left_value, right_value):
     left_index = bisect_left(array, left_value)
     right_index = bisect_right(array, right_value)
 
-    return right_value - left_value
+    return right_index - left_index
 
 N, x = map(int, input().split())
 array = list(map(int, input().split()))
