265c15daa1a6b844813c61f236cd298ed61e5095
april2nds <rlawnsgh8395@naver.com>
2022-04-20 23:46:02 +0900

    0420 practice

---
+++ BOJ_Practice/Dinamic_prog/BOJ_12865.py
@@ -0,0 +1,14 @@
+# 평범한 배낭
+## K의 무게를 버티는 배낭에 가치가 V인 물건 N개를 입력
+## 가치의 최댓값을 출력
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+d = [0 for _ in range(k + 1)]
+
+for i in range(n):
+    w, v = map(int, input().split())
+
+    for j in range()
