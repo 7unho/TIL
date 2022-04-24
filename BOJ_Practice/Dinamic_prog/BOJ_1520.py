b8abe0866e1a5fde229baf18623129d207dd2967
april2nds <rlawnsgh8395@naver.com>
2022-04-24 18:33:12 +0900

    0424 practice

--- BOJ_Practice/Dinamic_prog/BOJ_1520.py
+++ BOJ_Practice/Dinamic_prog/BOJ_1520.py
@@ -2,15 +2,37 @@
 ## 인접한 노드 중 현재 노드보다 작은 값을 갖는 노드로만 이동할 때,
 ## 도착지까지의 경우의 수
 
+def dfs(x, y):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    if x < 0 or y < 0 or x >= n or y >= m:
+        return False
+    if not visited[x][y]:
+        visited[x][y] = 1
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+
+            if graph[x][y] > graph[nx][ny]:
+                dfs(nx, ny)
+                return True
+    return False
+
 import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0] * m for _ in range(n)]
+answer = 0
 
-for i in range(n):
-    graph[i].insert(0, 0)
-graph.insert(0, [0 for _ in range(m + 1)])
+if dfs(0, 0):
+    answer += 1
 
-d = [[0] * (m + 1) for _ in range(n + 1)]
-
+print(answer)
+print(visited)
