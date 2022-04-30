2baabb74499bceaf26c30e4420a768f91feda24a
april2nds <rlawnsgh8395@naver.com>
2022-04-30 15:17:04 +0900

    0430 practice

--- BOJ_Practice/Greedy/BOJ_2437.py
+++ BOJ_Practice/Greedy/BOJ_2437.py
@@ -1,20 +1,17 @@
-# # 저울
-# ## 입력값 : 측정하려는 값 , 출력값 : 정해진 저울들의 조합으로 할 수 없는 최솟값
+# 저울
+## 입력값 : 측정하려는 값 , 출력값 : 정해진 저울들의 조합으로 할 수 없는 최솟값
 
-# import sys
-# input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
-# n = int(input())
-# weights = list(map(int, input().split()))
-# weights.sort()
-# answer = 1
+n = int(input())
+weights = list(map(int, input().split()))
+weights.sort()
+answer = 1
 
-# for i in range(1, sum(weights) + 2):
-#     if i in weights:
-#         continue
+for i in weights:
+    if answer < i:
+        break
+    answer += i
 
-#     if i - 
-
-test = [1, 1, 3, 5, 7]
-test = list((lambda x : x if x > 4 else 0))
-print(test)
+print(answer)
