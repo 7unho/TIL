406df4843d292a85a54984ae5c84197c9e64e483
april2nds <rlawnsgh8395@naver.com>
2022-05-10 17:45:58 +0900

    0510 practice

--- BOJ_Practice/DFS_BFS/BOJ_1697.py
+++ BOJ_Practice/DFS_BFS/BOJ_1697.py
@@ -1,30 +1,22 @@
 # 숨바꼭질
 ## 입력값 : N (술래의 위치), K (타겟의 위치) 출력값 : 타겟을 찾는 가장 빠른 시간
 
-def make_graph(x):
-    graph = [x - 1, x + 1, x * 2]
-    return graph
-
 from collections import deque
 import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-
 queue = deque([n])
-visited = []
-
+dist = [0] * (10 ** 6 + 1)
 while queue:
     v = queue.popleft()
-    print(v, end=' ')
     if v == k:
+        print(dist[v])
         break
 
-    for point in make_graph(v):
-        if point <= 0 or point >= 10 ** 6:
-            continue
+    for node in [v - 1, v + 1, v * 2]:
+        if 0 <= node <= 10 ** 6 and not dist[node]:
+            dist[node] = dist[v] + 1
+            queue.append(node)
 
-        if point not in visited:
-            visited.append(point)
-            queue.append(point)
-
+print(dist)
