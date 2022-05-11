48d8be8bb32bbc4c33b207abe702dc7ec73bdaf2
april2nds <rlawnsgh8395@naver.com>
2022-05-11 18:58:44 +0900

    0511 practice

--- BOJ_Practice/DFS_BFS/BOJ_14502.py
+++ BOJ_Practice/DFS_BFS/BOJ_14502.py
@@ -10,46 +10,50 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
+graph = []
 queue = deque([])
 answer = 0
 
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+
+    for j in range(m):
+        if graph[i][j] == 2:
+            queue.append((i, j))
 
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
-
-def start_virus():
-    global answer
-    zero_cnt = 0
-    
-    copy_graph = deepcopy(graph)
-
+def count_zero(graph):
+    sum = 0
     for i in range(n):
-        for j in range(m):
-            if copy_graph[i][j] == 2:
-                queue.append((i, j))
+        sum += graph[i].count(0)
+    
+    return sum
 
-    while queue:
-        x, y = queue.popleft()
+def start_virus(graph):
+    global answer
+
+    virus_list = deepcopy(queue)
+    while virus_list:
+        x, y = virus_list.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if nx < 0 or ny < 0 or nx >= n or ny >= m:
                 continue
-            if copy_graph[nx][ny] == 0:
-                copy_graph[nx][ny] = 2
-                queue.append((nx, ny))
+            if graph[nx][ny] in [1, 2]:
+                continue
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = 2
+                virus_list.append((nx, ny))
     
-    for i in range(n):
-        zero_cnt += copy_graph[i].count(0)
-
-    answer = max(answer, zero_cnt)
+    answer = max(answer, count_zero(graph))
 
 def make_wall(x):
     if x == 3:
-        start_virus()
-        return
+        copy_graph = deepcopy(graph)
+        return start_virus(copy_graph)
     
     for i in range(n):
         for j in range(m):
