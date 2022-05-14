20ff05d7d86194a8dcff4f03a6bbf06304f61b82
april2nds <rlawnsgh8395@naver.com>
2022-05-14 14:49:21 +0900

    0514 practice

--- BOJ_Practice/DFS_BFS/BOJ_2206.py
+++ BOJ_Practice/DFS_BFS/BOJ_2206.py
@@ -10,36 +10,18 @@ n, m = map(int, input().split())
 
 graph = [list(map(int, input().rstrip())) for _ in range(n)]
 
+cnt_graph = [[[0] * 2 for _ in range(m)] for _ in range(n)]
+cnt_graph[0][0][0] = 1
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, 1, -1]
 
 queue = deque()
-queue.append([0, 0])
+queue.append([0, 0, 0])
 
-answers = []
-
-def break_wall(cnt):
-    if cnt == 1:
-        copy_graph = deepcopy(graph)
-        bfs(copy_graph)
-        return
-    
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 1:
-                graph[i][j] = 0
-                break_wall(cnt + 1)
-                graph[i][j] = 1
-
-
-def bfs(graph):
-    global answer
-
-    cnt_graph = [[0] * m for _ in range(n)]
-    cnt_graph[0][0] = 1
-
+def bfs():
     while queue:
-        x, y = queue.popleft()
+        x, y, z = queue.popleft()
 
         for i in range(4):
             nx = x + dx[i]
@@ -48,14 +30,15 @@ def bfs(graph):
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
 
-            if graph[nx][ny] == 0 and cnt_graph[nx][ny] == 0:
-                cnt_graph[nx][ny] = cnt_graph[x][y] + 1
-                queue.append([nx, ny])
-    
-    if cnt_graph[n - 1][m - 1] != 0:
-        answers.append(cnt_graph[n - 1][m - 1])
+            if graph[nx][ny] == 1 and z == 0:
+                cnt_graph[nx][ny][1] = cnt_graph[x][y][0] + 1
+                queue.append([nx, ny, 1])
+            elif graph[nx][ny] == 0 and cnt_graph[nx][ny][z] == 0:
+                cnt_graph[nx][ny][z] = cnt_graph[x][y][z] + 1
+                queue.append([nx, ny, z])
 
-break_wall(0)
+    return cnt_graph[n - 1][m - 1][z]
 
-answer = min(answers) if len(answers) >= 1 else -1
-print(answer)
+answer = bfs()
+print(answer if answer != 0 else -1)
+
