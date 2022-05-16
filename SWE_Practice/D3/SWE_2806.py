4f37cf72fdf4ce59b1310a3a4ab27103632cd2a1
april2nds <rlawnsgh8395@naver.com>
2022-05-16 16:35:26 +0900

    0516 practice

--- SWE_Practice/D3/SWE_2806.py
+++ SWE_Practice/D3/SWE_2806.py
@@ -1,47 +1,28 @@
-# N-Queen
-import sys
-n = int(input())
-
-graph = [[0] * n for _ in range(n)]
-answer = 0
-
-dx = [1, 1, -1, -1]
-dy = [-1, 1, 1, -1]
-
-
-def check_queen(x, y):
-
-    # 가로 체크
-    if graph[x].count(1) != 0:
-        return False
-
-    # 세로 체크
-    for i in range(n):
-        if graph[i][y] == 1:
-            return False
+def make_queen(graph, cnt):
+    global answer, n
     
-    # 대각선 체크...
-        
-
-
-
-def make_queen(cnt):
-    global answer 
-
     if cnt == n:
-        return True
-    
-    for i in range(n):
-        for j in range(n):
-            if graph[i][j] == 0 and check_queen(i, j):
-                graph[i][j] = 1
-                make_queen(cnt + 1)
-                graph[i][j] = 0
-                answer += 1
+        answer += 1
+        return
 
-make_queen(0)
-print(answer)
+    temp = [0] * n
 
+    for i in range(len(graph)):
+        temp[graph[i]] = 1
 
+        if graph[i] - (cnt - i) >= 0:
+            temp[graph[i] - (cnt - i)] = 1
+        
+        if graph[i] + (cnt - i) < n:
+            temp[graph[i] + (cnt - i)] = 1
 
+    for j in range(n):
+        if temp[j] == 0:
+            make_queen(graph + [j], cnt + 1)
+        
+for i in range(int(input())):
+    n = int(input())
+    answer = 0
+    make_queen([], 0)
 
+    print(f"#{i + 1} {answer}")
