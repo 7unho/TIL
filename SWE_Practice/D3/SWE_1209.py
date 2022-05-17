1240b78da886a6f010dd522ced68ef3990bd0d43
april2nds <rlawnsgh8395@naver.com>
2022-05-17 19:35:06 +0900

    0517 practice

--- SWE_Practice/D3/SWE_1209.py
+++ SWE_Practice/D3/SWE_1209.py
@@ -4,22 +4,23 @@
 
 answer = 0
 
-for tc in range(int(input())):
-    graph = [list(map(int, input().split())) for _ in range(5)]
+for tc in range(1, 11):
+    t = int(input())
+    graph = [list(map(int, input().split())) for _ in range(100)]
     cross_sum, rev_cross_sum = 0, 0
     rows_max, cols_max, cols_sum = 0, 0, 0
 
-    for i in range(5):
+    for i in range(100):
         rows_max = max(rows_max, sum(graph[i]))
         cols_max = max(cols_max, cols_sum)
         cols_sum = 0
         cross_sum += graph[i][i]
-        rev_cross_sum += graph[i][4 - i]
+        rev_cross_sum += graph[i][99 - i]
 
-        for j in range(5):
+        for j in range(100):
             cols_sum += graph[j][i]
     
     print(rows_max, cols_max, rev_cross_sum, cross_sum)
     answer = max(cross_sum, rev_cross_sum, rows_max, cols_max)
 
-    print(f"#{tc + 1} {answer}")
+    print(f"#{tc} {answer}")
