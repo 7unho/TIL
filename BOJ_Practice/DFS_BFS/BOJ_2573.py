c2e58087acd00522784690e94add7d8aa04dff40
april2nds <rlawnsgh8395@naver.com>
2022-05-22 08:11:24 +0900

    0522 practice

--- BOJ_Practice/DFS_BFS/BOJ_2573.py
+++ BOJ_Practice/DFS_BFS/BOJ_2573.py
@@ -1,25 +1,32 @@
 # 빙산
 
-## 빙산 덩어리 체크용 dfs 함수
-def dfs(x, y):
-    if x < 0 or x >= n or y < 0 or y >= m:
-        return False
+def check_graph():
+    for i in range(n):
+        if graph[i].count(0) < m:
+            return False
+    return True
 
-    if dfs_graph[x][y] > 0:
-        dfs_graph[x][y] = 0
+def bfs():
+    while queue:
+        x, y = queue.popleft()
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            dfs(nx, ny)
-        return True
-    return False
+            if nx < 1 or ny < 1 or nx >= n - 1 or ny >= m - 1:
+                continue
 
+            if not visited[nx][ny] and graph[nx][ny] != 0:
+                visited[nx][ny] = True
+                bfs_graph[nx][ny] = 0
+                queue.append([nx, ny])
+
+from collections import deque
 from copy import deepcopy
 import sys
-sys.setrecursionlimit(10 ** 6)
 
+input = sys.stdin.readline
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 
@@ -28,18 +35,41 @@ dy = [1, -1, 0, 0]
 
 answer = 0
 
-for _ in range(max(n, m)):
-    dfs_graph = deepcopy(graph)
+while True:
+    if check_graph():
+        answer = 0
+        break
+
+    bfs_graph = deepcopy(graph)
+    melt_graph = deepcopy(graph)
+    visited = [[False] * m for _ in range(n)]
     ice_grp = 0
 
     for i in range(1, n - 1):
         for j in range(1, m - 1):
-            if dfs(i, j):
+            if not visited[i][j] and graph[i][j] > 0:
+                visited[i][j] = True
+                queue = deque()
+                queue.append([i, j])
+                bfs()
                 ice_grp += 1
-    
-    if ice_grp >= 2:
+
+            for k in range(4):
+                nx = i + dx[k]
+                ny = j + dy[k]
+
+                if melt_graph[i][j] == 0:
+                    continue
+
+                if melt_graph[nx][ny] == 0:
+                    graph[i][j] = graph[i][j] - 1 if graph[i][j] > 0 else 0
+    if ice_grp > 1:
         break
 
+    for i in range(n):
+        print(bfs_graph[i])
     answer += 1
     
-print(answer if ice_grp > 2 else 0)
+
+
+print(answer, ice_grp)
