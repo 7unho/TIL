2ebf1b6b77b0618d88c9211d2b61e0f275cf463a
april2nds <rlawnsgh8395@naver.com>
2022-06-07 20:09:32 +0900

    0607 practice;

--- BOJ_Practice/Dinamic_prog/BOJ_1520.py
+++ BOJ_Practice/Dinamic_prog/BOJ_1520.py
@@ -1,38 +1,34 @@
 # 내리막길
-## 인접한 노드 중 현재 노드보다 작은 값을 갖는 노드로만 이동할 때,
-## 도착지까지의 경우의 수
 
 def dfs(x, y):
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-
     if x < 0 or y < 0 or x >= n or y >= m:
         return False
+    
     if not visited[x][y]:
-        visited[x][y] = 1
+        visited[x][y] = True
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
-
-            if graph[x][y] > graph[nx][ny]:
+            if (0 <= nx < n) and (0 <= ny < m) and (graph[x][y] > graph[nx][ny]):
+                dp[nx][ny] = max(dp[nx][ny], dp[nx][ny] + 1)
                 dfs(nx, ny)
-                return True
-    return False
+        
+    pass
 
 import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-visited = [[0] * m for _ in range(n)]
-answer = 0
+visited = [[False] * m for _ in range(n)]
+dp = [[0] * m for _ in range(n)]
 
-if dfs(0, 0):
-    answer += 1
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
-print(answer)
-print(visited)
+dfs(0, 0)
+
+for i in range(n):
+    print(dp[i])
