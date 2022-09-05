921d650c0daee3ac51f07ee6febda6f7989c2bdb
april2nds <rlawnsgh8395@naver.com>
2022-09-05 21:36:12 +0900

    [ADD] 220905 practice

---
+++ Algo_Study/Programmers/PG_118668_코테공부.py
@@ -0,0 +1,14 @@
+# result 15
+alp = 10
+cop = 10
+problems = [[10,15,2,1,2],[20,20,3,3,4]]
+
+## result 13
+# alp = 0
+# cop = 0
+# problems = [[0,0,2,1,2],[4,5,3,1,2],[4,11,4,0,2],[10,4,0,4,2]]
+
+
+def solution(alp, cop, problems):
+    answer = 0
+    return answer
