3b715a191d35986d66e9976f7d2f3ce8be2d97e7
april2nds <rlawnsgh8395@naver.com>
2022-09-26 19:55:58 +0900

    [ADD] 220926 practice

---
+++ SSAFY/220926/sql/schema.sql
@@ -0,0 +1,14 @@
+-- Active: 1663060283928@@127.0.0.1@3306@ssafydb
+
+create SCHEMA `aptcrawl`;
+
+show databases;
+
+use aptcrawl;
+
+show tables;
+
+
+select * from dongcode where gugunName = '강서구';
+
+select * from houseinfo where dong = '등촌동';
